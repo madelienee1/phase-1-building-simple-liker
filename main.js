913b173ea4df52fff80bc27cd017b620ebe08629
Madeliene <madelienestiller@gmail.com>
2023-11-19 10:37:23 +1000

    liker app complete

--- main.js
+++ main.js
@@ -4,6 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//Select the glyphs
+const likeGlyphs = document.querySelectorAll('.like-glyph');
+
+// Function to toggle heart
+function toggleHeart(heart) {
+  if (heart.textContent === EMPTY_HEART) {
+    mimicServerCall()
+      .then(() => {
+        heart.textContent = FULL_HEART;
+        heart.classList.add('activated-heart');
+      })
+      .catch(error => {
+        const modal = document.getElementById('modal');
+        const modalMessage = document.getElementById('modal-message');
+        modalMessage.textContent = error;
+        modal.classList.remove('hidden');
+
+        setTimeout(() => {
+          modal.classList.add('hidden');
+        }, 3000);
+      });
+  }
+  else {
+    heart.textContent = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+  }
+}
+
+//Add event listeners to like glyphs
+
+likeGlyphs.forEach(heart => {
+  heart.addEventListener('click', () => toggleHeart(heart));
+});
 
 
 
